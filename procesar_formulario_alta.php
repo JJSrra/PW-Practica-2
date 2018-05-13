fe57243309dc787c9e2735430f88a257bbb02590
Juanjo Sierra <jjsierra103@gmail.com>
2018-05-13 16:46:02 +0200

    Generada página de redirección tras alta de usuario

--- procesar_formulario_alta.php
+++ procesar_formulario_alta.php
@@ -21,13 +21,10 @@ $sentencia = $conexion->prepare($sql);
 try {
 
     foreach($_POST as $key => $value) {
-        echo $key." ";
         if (!empty($value)){
-            echo $value." ";
             $sentencia->bindValue(":".$key, $value);
         }
         else{
-           echo "Campo no asignado ";
            $sentencia->bindValue(":".$key, 0);
         }
     }
@@ -38,4 +35,36 @@ try {
     die("Petición fallida: ".$e->getMessage());
 }
 
+$nombre = $_POST['nombre'];
+
 ?>
+
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Centro Deportivo - Página principal</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+
+<header>
+    <a href="index2.html">
+        <img src="imagenes/trophy.svg" />
+    </a>
+    <h1 class="title">Centro deportivo de PW</h1>
+    <div class="sign-out">
+        <p>Bienvenid@ Usuario123</p>
+        <a href="index.html">
+            <p>Desconectarse</p>
+        </a>
+    </div>
+</header>
+
+<body>
+    <section class="registro">
+        <h2 class="mensaje-registro">¡Gracias por registrarte, <?php echo $nombre; ?>!</h2>
+        <h4 class="mensaje-registro">Puedes volver a la página principal siguiendo <a href="index2.html">este enlace</a>.</h4>
+    </section>
+</body>
+</html>
