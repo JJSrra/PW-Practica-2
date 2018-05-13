b49bfe717caa2cb05f0511eb1e3b404aceaf160b
Juanjo Sierra <jjsierra103@gmail.com>
2018-05-13 12:59:49 +0200

    Actualizado formulario y script de procesamiento para guardar nuevos usuarios en la base de datos

--- procesar_formulario_alta.php
+++ procesar_formulario_alta.php
@@ -1,13 +1,41 @@
 <?php
 
-foreach($_POST as $key => $value) {
-    echo $key." ";
-    if (!empty($value)){
-        echo $value." ";
+define("DB_DSN", "mysql:host=localhost;dbname=db76589592_pw1718" );
+define("DB_USUARIO", "x76589592");
+define("DB_PASSWORD", "76589592");
+
+try {
+    $conexion = new PDO(DB_DSN, DB_USUARIO, DB_PASSWORD);
+    $conexion->query('SET NAMES utf8');
+} catch (PDOException $e){
+    die("Conexión fallida: ".$e->getMessage());
+}
+
+$sql = "INSERT INTO Usuarios VALUES (:nombre, :primerApellido, :segundoApellido, :nickname,
+    :passphrase, :correoElectronico, :fechaNacimiento, :actividadPreferida, :apuntarseFutbol,
+    :apuntarseBaloncesto, :apuntarseRugby, :apuntarseZumba, :apuntarseCrossFit, :apuntarseYoga,
+    :apuntarseSpinning, :apuntarseKickboxing, :miembroAnterior, :conocimiento)";
+
+$sentencia = $conexion->prepare($sql);
+
+try {
+
+    foreach($_POST as $key => $value) {
+        echo $key." ";
+        if (!empty($value)){
+            echo $value." ";
+            $sentencia->bindValue(":".$key, $value);
+        }
+        else{
+           echo "Campo no asignado ";
+           $sentencia->bindValue(":".$key, 0);
+        }
     }
-    else{
-       echo "Campo no asignado ";
-    }
+
+
+    $sentencia->execute();
+} catch (PDOException $e){
+    die("Petición fallida: ".$e->getMessage());
 }
 
 ?>
