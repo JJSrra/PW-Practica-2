d821c77ca7480a5841f0d1ff407b9d93704b997d
Juanjo Sierra <jjsierra103@gmail.com>
2018-05-13 19:11:07 +0200

    Añadida página de registrado por separado

---
+++ registrado.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Centro Deportivo - Página principal</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+
+<header>
+    <a href="index2.html">
+        <img src="imagenes/trophy.svg" />
+    </a>
+    <h1 class="title">Centro deportivo de PW</h1>
+    <div class="sign-out">
+        <p>Bienvenid@ Usuario123</p>
+        <a href="index.html">
+            <p>Desconectarse</p>
+        </a>
+    </div>
+</header>
+
+<body>
+    <section class="registro">
+        <h2 class="mensaje-registro">¡Gracias por registrarte!</h2>
+        <h4 class="mensaje-registro">Puedes volver a la página principal siguiendo
+            <a href="index2.html">este enlace</a>.</h4>
+    </section>
+</body>
+
+</html>
