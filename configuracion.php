e19a266703f9d2877e19128b953521a1d1378ca7
Juanjo Sierra <jjsierra103@gmail.com>
2018-05-13 20:21:08 +0200

    Añadidos ficheros de configuración para conexiones PHP

---
+++ configuracion.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_DSN", "mysql:host=localhost;dbname=db76589592_pw1718" );
+define("DB_USUARIO", "x76589592");
+define("DB_PASSWORD", "76589592");
+define("TABLA_USUARIOS", "Usuarios");
+
+?>
