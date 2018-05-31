0bf763e003872624690dee14e81824de0f5fed19
Juanjo Sierra <jjsierra103@gmail.com>
2018-05-31 18:28:26 +0200

    Corregido error en login de usuario

--- procesar_login.php
+++ procesar_login.php
@@ -4,8 +4,7 @@ session_start();
 
 require_once("usuarios.class.inc.php");
 
-$usuario = new Usuario();
-$validado = $usuario->validarLogin($_POST['nombreUsuario'], $_POST['passwordUsuario']);
+$validado = Usuario::validarLogin($_POST['nombreUsuario'], $_POST['passwordUsuario']);
 
 if ($validado) {
     $_SESSION['nickname'] = $_POST['nombreUsuario'];
